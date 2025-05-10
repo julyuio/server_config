9401357022e859acc0d4350e17245eb83c55c505
july <iulian.marinescu.jobs@gmail.com>
2025-05-10 17:28:21 +0000

    v0.3

--- 60_Nginx.sh
+++ 60_Nginx.sh
@@ -27,6 +27,29 @@ fi
 read -p "Installing Password Manager (Passmag) ? (yes/no): " response
 if [ "$response" == "yes" ] || [ -z "$response" ]; then
         echo "Proceeding..."
+	#some config
+	codedir="/code"
+	appdir="/code/passmag"
+
+	#creating directories
+	mkdir $codedir
+	mkdir $appdir
+	sleep 10
+	#cd /code/passmag
+	git clone https://github.com/julyuio/Pass  /code/passmag
+
+	#installing virtual env
+	apt install python3.10-venv -y
+	echo "Creating Venv..." && sleep 2
+	sudo python3 -m venv /code/passmag/venv
+	echo "Activating Venv..." && sleep 2
+        source /code/passmag/venv/bin/activate
+	echo "Installing Requirements..." && sleep2
+	pip install -r /code/passmag/requirements.txt
+	cp file.gconfig.py /code/passmag/gconfig.py
+	echo ""
+	echo "In order to activate gunicorn use take a look at gconfig.py and also use the folowing: "
+	echo -e "${IGreen} gunicorn -c /code/passmag/gconfig.py Pass.wsgi:application ${Color_Off}"
 else
         echo "Skipping PassMag..."
         exit 1
---
+++ file.gconfig.py
@@ -0,0 +1,6 @@
+bind = "127.0.0.1:8981"
+workers = 4
+max_requests = 5000
+errorlog = "error.log"
+loglevel = 'debug'
+accesslog = "access.log"
