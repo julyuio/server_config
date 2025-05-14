e6e393d3b9197f362dd873568ec84dfdd8e89b58
july <iulian.marinescu.jobs@gmail.com>
2025-05-14 16:02:05 +0000

    v1

--- file.crontab_ufw_6hours.sh
+++ file.crontab_ufw_6hours.sh
@@ -5,6 +5,8 @@ tempdir="/july/iplog"
 tempfile="$tempdir/temp_ips.txt"
 ip_block="$tempdir/block_list.txt"
 ip_allow="$tempdir/allow_list.txt"
+stat_file="$tempdir/stats.txt"
+ip_log="$tempdir/log"
 
 if [ ! -d "$tempdir" ]; then
     mkdir -p $tempdir
@@ -18,12 +20,15 @@ ports=(22 80 443 53 6195 51280 51281 51282 51283 51284 51285 51286 51287 51288 5
 #___________ Extract IPs__________
 echo "Adding  unsecured ssh tries..."
 sudo journalctl --since="1 days ago" -u ssh | grep -e "banner exchange" -e "fail" |grep -E -o '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)' >> $tempfile
+stat_ssh_tries=$(wc -l < $tempfile)
 
 echo "Extracting IPs excluding HTTP access codes: 40x and 50x"
 awk '$9 ~ /^40[0-9]/ || $9 ~ /^50[0-9]/  {print $1}'  /var/log/nginx/access.log | sort -ur  | grep -v '10.0.0' | grep -v '172.16.0' >> $tempfile
+stat_total=$(wc -l < $tempfile)
 
 #____________ Block IPs___________
 # Read the file line by line
+echo "Blocking IPs.."
 while IFS= read -r line
 do
   #echo "Denying IP: $line"
@@ -31,7 +36,7 @@ do
 done < "$tempfile"
 
 #____________ Allow IPs___________
-
+echo "Allowing own IPs and ports..."
 #Setting internal rules
 ufw allow from 10.0.0.0/12  > /dev/null 2>&1
 ufw allow from 172.16.0.0/12 > /dev/null 2>&1
@@ -54,10 +59,34 @@ do
   ufw allow from "$line" > /dev/null 2>&1
 done < "$ip_allow"
 
+#________ Statistics_________
+
+#actually the nginx stat are the total minus the ssh tries at the begining
+stat_nginx=$(expr $stat_total - $stat_ssh_tries)
+datetime=$(date +"%d-%m-%Y %H:%M:%S")
+stat_iptable=$(iptables -L | wc -l)
+
+if [ ! -f "$stat_file" ]; then
+    echo "Date Time Total SSH Nginx IPtables" > $stat_file
+fi
+
+echo "$datetime Total:$stat_total SSH:$stat_ssh_tries Nginx:$stat_nginx IPtables:$stat_iptable"
+echo "$datetime $stat_total $stat_ssh_tries $stat_nginx $stat_iptable" >> $stat_file
+
+
+#create and save the ips in a day file in log directory
+if [ ! -d "$ip_log" ]; then
+    mkdir -p $ip_log
+fi
+logfile="$ip_log/blocked_$(date +%d-%m-%Y).txt"
+cat $tempfile > $logfile
+
+
 #__________Clean up______________
 ufw reload
 
 #Adding today's IPs to the the main ip file
 cat $tempfile >> $ip_block
 rm $tempfile
-
+echo "Done"
+echo ""
