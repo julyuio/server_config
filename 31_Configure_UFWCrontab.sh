87c05c16227ad1f31db6095bea671c0bdd8ff929
julyuio <157488522+julyuio@users.noreply.github.com>
2025-06-04 15:21:38 +0100

    Update 31_Configure_UFWCrontab.sh

--- 31_Configure_UFWCrontab.sh
+++ 31_Configure_UFWCrontab.sh
@@ -2,7 +2,7 @@
 
 source conf
 
-tempdir="/july/crotab"
+tempdir="/july/crontab"
 
 #______________Check if python & jq are  installed____________
 
@@ -35,18 +35,18 @@ if [ ! -d "$tempdir" ]; then
     mkdir -p $tempdir
 fi
 
-cp file.crontab_ufw.sh  $tempdir/crontab_ufw.sh
+cp file.crontab_ufw.sh  $tempdir/crontab_ufw_monthly.sh
 cp file.crontab_ufw_6hours.sh $tempdir/crontab_ufw_daily.sh
 touch $tempdir/allow_list.txt
 touch $tempdir/ block_list.txt
 cp conf $tepdir/conf
 cp kompress_ipv4.py $tempdir/kompress_ipv4.py
-chmod 775 $tempdir/crontab_ufw.sh
+chmod 775 $tempdir/crontab_ufw_monthly.sh
 chmod 775 $tempdir/crontab_ufw_daily.sh
 
 echo -e " Run command :${IRed} sudo crontab -e ${Color_Off} and copy the folowing line at the end of the file: "
 echo -e " ${IGreen}45  23  *   *   *    $tempdir/crontab_ufw_daily.sh${Color_Off}"
-echo -e " ${IGreen}45  23  1   *   *    $tempdir/crontab_ufw.sh${Color_Off}"
+echo -e " ${IGreen}45  23  1   *   *    $tempdir/crontab_ufw_monthly.sh${Color_Off}"
 echo " "
 
 
